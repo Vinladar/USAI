bb4be869bb13139733b46e0c1281326265055678
Bryan Paulsen <bryanpaulsen@comcast.net>
2020-09-03 16:22:14 -0400

    Final code update and report generation.

--- Code/Scripts/metricsReport.R
+++ Code/Scripts/metricsReport.R
@@ -17,8 +17,9 @@ reasonCodes <- read.csv("Code/supportFiles/Reasoncode.csv")
 driver_to_partFam <- read.csv("Code/supportFiles/driverToPartFam.csv")
 light_engines <- read.csv("Code/supportFiles/LightEngines.csv")
 light_engine_to_partFam <- read.csv("Code/supportFiles/LightEngineToPartFam.csv")
+LEM_to_LED <- read_excel("Code/supportFiles/LEM_to_LED.xlsx")
 
-rma <- loadData("Code/Failure_rate/July_2020.xlsx")
+rma <- loadData("Code/Failure_rate/August_2020.xlsx")
 rma2 <- select(rma, RMA.ID, Item.ID, Item.Name, Return.Qty, Reason.Code)
 rma3 <- merge(rma2, reasonCodes, by.x = c("Reason.Code"), by.y = c("reason"), all.x = TRUE)[c(2:6)]
 rma3 <- rename(rma3, Reason.Code = code)
@@ -27,28 +28,20 @@ getDrivers <- function(rma) {
   rmaDrivers <- rma[grep("SP-[0-9]{3}-[0-9]{4}", rma$Item.ID),]
   groupedDrivers <- merge(rmaDrivers, driver_to_E2, by.x = c("Item.ID"), by.y = c("SP_Kit"), all.x = TRUE) %>%
     group_by(E2)
-# groupedDrivers <- summarize(groupedDrivers, Qty = sum(Return.Qty))
-  splitDrivers <- group_split(groupedDrivers)
-  splitDrivers <- lapply(splitDrivers, function(x) {
-    cbind(x, Group = factor(1:nrow(x)))
-  })
-  groupedDrivers <- rbindlist(splitDrivers)[,c(2, 3, 4)]
-  g <- ggplot(data = groupedDrivers, 
-              aes(x = reorder(E2, -Return.Qty), y = Return.Qty, position = "stacked", fill = Group))
-  g + geom_bar(stat = "identity") + 
-    labs(x = "Driver") +
-    theme(axis.text.x = element_text(angle = 90), legend.position = "none") +
-    scale_fill_brewer(palette="Set1")
+  groupedDrivers <- groupedDrivers[, c(2, 4, 6)]
+  groupedDrivers1 <- summarise(groupedDrivers, "# of RMAs" = length(unique(RMA.ID)),  Qty = sum(Return.Qty))
+  groupedDrivers1 <- groupedDrivers1[, c(2, 3, 1)]
+  groupedDrivers1 <- arrange(groupedDrivers1, desc(Qty))
+  write.csv(groupedDrivers1, "Code/Output/August_2020/Drivers.csv")
 }
 
 getLightEngines <- function(rma) {
   rmaEngines <- rma[grep("LEM-", rma$Item.ID),]
-  rmaEngines <- cbind(rmaEngines, Item.ID = substring(rmaEngines$Item.ID, 1, 7))[,c(6, 4, 1)]
-  groupedEngines <- group_by(rmaEngines, Item.ID)
-  groupedEngines <- summarize(groupedEngines, "# of RMAs" = length(unique(RMA.ID)),  Qty = sum(Return.Qty))
+  rmaEngines <- merge(rmaEngines, LEM_to_LED, by.x = c("Item.ID"), by.y = c("LEM_Kit"), all.x = TRUE)
+  groupedEngines <- group_by(rmaEngines, LED)
+  groupedEngines <- summarize(groupedEngines, "# of RMAs" = length(unique(RMA.ID)),  Qty = sum(Return.Qty))[, c(2, 3, 1)]
   groupedEngines <- arrange(groupedEngines, desc(Qty))
-  g <- ggplot(data = groupedEngines, aes(x = reorder(Item.ID, -Qty), y = Qty))
-  g + geom_bar(stat = "identity") + labs(x = "Light Engine") + geom_text(aes(label = Qty, y = Qty + 2.0), position = position_dodge(0.9), vjust = 0)
+  write.csv(groupedEngines, "Code/Output/August_2020/Engines.csv")
 }
 
 getReasonCodes <- function(rma) {
@@ -56,11 +49,10 @@ getReasonCodes <- function(rma) {
   groupedCodes <- group_by(codes, Reason.Code) %>%
     summarize(Number_of_RMAs = length(unique(RMA.ID)), Qty = sum(Return.Qty)) %>%
     arrange(desc(Qty))
-  g <- ggplot(data = groupedCodes, aes(x = reorder(Reason.Code, -Number_of_RMAs), y = Number_of_RMAs))
-  g + geom_bar(stat = "identity") + labs(x = "Reason Code") + geom_text(aes(label = Number_of_RMAs, y = Number_of_RMAs + 2.0), position = position_dodge(0.9), vjust = 0)
+  write.csv(groupedCodes, "Code/Output/August_2020/Codes.csv")
 }
 
-generateGraphs <- function(rma) {
+generateReports <- function(rma) {
   getDrivers(rma)
   getLightEngines(rma)
   getReasonCodes(rma)
