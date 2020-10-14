1f806898312e575d2c940af25670fb44c9a2bfd0
Bryan Paulsen <bryanpaulsen@comcast.net>
2020-10-14 14:09:54 -0400

    New Script added

--- Misc/Scripts/Credit_memo_Totals.R
+++ Misc/Scripts/Credit_memo_Totals.R
@@ -6,9 +6,11 @@
 #   extended prices in the remaining rows.
 library(openxlsx)
 
-raw_data <- read.xlsx("Misc/Credit_Memo_RMAs_2019_to_10_2020.xlsx")
-clean_data <- raw_data[, c(1, 3, 5, 7, 9, 11, 12, 13, 21, 22, 23, 24, 25, 30, 32, 33, 37)]
-no_restock <- clean_data[clean_data$Restocking.Percent == "",]
-no_restock_credit_memo <- no_restock[grep("..", no_restock$`Credit.Memo.Date./.ID`), ]
-no_restock_credit_memo$Extended.Price <- as.numeric(no_restock_credit_memo$Extended.Price)
-total <- sum(no_restock_credit_memo$Extended.Price, na.rm = TRUE)
+calculate_totals <- function(Data, Output) {
+  clean_data <- raw_data[, c(1, 3, 5, 7, 9, 11, 12, 13, 21, 22, 23, 24, 25, 30, 32, 33, 37)]
+  no_restock <- clean_data[clean_data$Restocking.Percent == "",]
+  no_restock_credit_memo <- no_restock[grep("..", no_restock$`Credit.Memo.Date./.ID`), ]
+  no_restock_credit_memo$Extended.Price <- as.numeric(no_restock_credit_memo$Extended.Price)
+  total <- sum(no_restock_credit_memo$Extended.Price, na.rm = TRUE)
+  write.xlsx(no_restock_credit_memo, Output)
+}
