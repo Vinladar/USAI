5d0626bcd46c2845592288398ee7b1488d7e1981
Bryan Paulsen <bryanpaulsen@comcast.net>
2020-10-14 16:38:10 -0400

    Updated credit memo reports

--- Misc/Scripts/Credit_memo_Totals.R
+++ Misc/Scripts/Credit_memo_Totals.R
@@ -7,10 +7,15 @@
 library(openxlsx)
 
 calculate_totals <- function(Data, Output) {
-  clean_data <- raw_data[, c(1, 3, 5, 7, 9, 11, 12, 13, 21, 22, 23, 24, 25, 30, 32, 33, 37)]
-  no_restock <- clean_data[clean_data$Restocking.Percent == "",]
-  no_restock_credit_memo <- no_restock[grep("..", no_restock$`Credit.Memo.Date./.ID`), ]
-  no_restock_credit_memo$Extended.Price <- as.numeric(no_restock_credit_memo$Extended.Price)
-  total <- sum(no_restock_credit_memo$Extended.Price, na.rm = TRUE)
-  write.xlsx(no_restock_credit_memo, Output)
+  clean_data <- Data[, c(1, 3, 5, 7, 9, 11, 12, 13, 21, 22, 23, 24, 25, 30, 32, 33, 37)]
+  names(clean_data) <- c("Customer Corp Name", "Sales Order ID", "Customer ID", "Return Qty", 
+                         "Customer PO Line Nbr",  "Discount Percentage", "Item Name", "Product Family", 
+                         "Problem Desc", "Credit Memo Date / ID", "Item ID", "Credit Memo ID", "Unit Price", 
+                         "RMA ID", "Extended Price", "Credit Memo Complete", "Restocking Percent")
+  no_restock <- clean_data[clean_data$`Restocking Percent` == "", ]
+  no_restock_credit_memo <- no_restock[grep("..", no_restock$`Credit Memo Date / ID`), ]
+  no_restock_credit_memo$`Extended Price` <- as.numeric(no_restock_credit_memo$`Extended Price`)
+  final <- no_restock_credit_memo[grep("DEFECTIVE", no_restock_credit_memo$`Problem Desc`), ]
+  total <- sum(final$`Extended Price`, na.rm = TRUE)
+  write.xlsx(final, Output)
 }
