ec6c84301ab9071240d49b20e70ec7863ddda015
Bryan Paulsen <bryanpaulsen@comcast.net>
2021-04-06 11:27:21 -0400

    April report done

--- Code/Scripts/rmaPrep.R
+++ Code/Scripts/rmaPrep.R
@@ -9,7 +9,6 @@ so_master <- read.xlsx("Code/supportFiles/Sales_Order_Master/SO_Master.xlsx")
 # Process the RMA information
 rma_edit <- function(rma, rmaDetails, outputString = "Default.xlsx") {
 	rma <- buildTable(rma, rmaDetails)
-	rma <- addReasonCodes(rma)
 	rmaOutput <- addProductFamilies(rma)
 	rmaOutput <- addUserDefs(rmaOutput)
 	write.xlsx(rmaOutput, outputString, asTable = FALSE)
@@ -20,20 +19,29 @@ rma_edit <- function(rma, rmaDetails, outputString = "Default.xlsx") {
 # Then the Dates are converted and the sales order numbers are added.
 # Column names are then redefined, error codes are added, then the summaries are formatted
 buildTable <- function(rma, rmaDetails) {
-	rma <- arrange(rma, desc(RMA.ID))
- 	rmaDetails <- arrange(rmaDetails, desc(RMA.ID))
+	names(rma) <- c("RMA_DateCreated", "CBA_Addr", "CBA_City", "CBA_Country", "Customer Name", 
+	                "CBA_Address", "RMA_Comments", "RMAL_Problem Description", "IMA_Prod Fam", 
+	                "RMAL_Return Quantity", "CBA_Currency Type", "IMA_Item ID", "CBA_EMail", 
+	                "CBA_Name", "RMA_ID", "CBA_Doc Delivery Method", "CBA_Fax")
+  rma <- arrange(rma, desc(RMA_ID))
+  names(rmaDetails) <- c("RMA_ID", "RMA Line", "SO Req'd Date", "SO_ID", "SO Line", "Invoice ID", 
+                         "Customer ID", "Item ID", "Rev", "Item Name", "Return Qty", "Unit Price", 
+                         "RMA Status", "Credit/Replace Option", "Reason_Code", "Disc. %", "Sales Tax", 
+                         "Extra Charges",  "Extra Charge Desc", "Extended Amt", "Project ID", 
+                         "Misc Line Description", "Problem Desc.", "Replacement Complete") 
+  rmaDetails <- arrange(rmaDetails, desc(RMA_ID))
 	rma[, c(2, 3, 4, 6, 11, 13, 14, 16, 17)] = ""
-	rma[,1] = convertToDate(rma[, 1], origin = "1900-01-01")
-	rma[,2] <- rmaDetails$SO.ID
+	## rma[,1] = convertToDate(rma[, 1], origin = "1900-01-01")
+	rma[,2] <- rmaDetails$SO_ID
 	columns <- c("Date", "orig_SO", "ShipDate", "Specifier", "Customer", 
 		"Job_Name", "RMA_Comments", "Summary", "Product_Family", "Qty", "Qty_On_Order", 
 		"Prob_Part", "Replaced", "Repl_SO", "RMA", "Comments", "Code")
 	colnames(rma) <- columns
-	rma$Code <- rmaDetails$Reason.Code
+	rma$Code <- rmaDetails$Reason_Code
 	rma <- rma[,-7]
 	rma$Summary <- as.data.table(strsplit(rma$Summary, " // "))[2] %>%
 		t()
-	rma$orig_SO <- rmaDetails$SO.ID
+	rma$orig_SO <- rmaDetails$SO_ID
 	rma
 }
 
@@ -51,9 +59,9 @@ addProductFamilies <- function(rma) {
 	drivers <- rma[grep("SP-[0-9]{3}-[0-9]{4}-", rma$Prob_Part),]
 	others <- rma
 	others <- others[-grep("LEM-|SP-[0-9]{3}-[0-9]{4}-", others$Prob_Part),]
-	engineToPartFam <- read.csv("H:/Code/supportFiles/LightEngineToPartFam.csv")
+	engineToPartFam <- read.csv("Code/supportFiles/LightEngineToPartFam.csv")
 	names(engineToPartFam) <- c("Item.ID", "Product.Family")
-	driverToPartFam <- read.csv("H:/Code/supportFiles/driverToPartFam.csv")
+	driverToPartFam <- read.csv("Code/supportFiles/driverToPartFam.csv")
 	names(driverToPartFam) <- c("Item.ID", "Product.Family")
 	engines <- cbind(engines, PartFam = substring(engines$Prob_Part, 1, 7))
 	drivers <- cbind(drivers, PartFam = substring(drivers$Prob_Part, 1, 6))
@@ -71,7 +79,7 @@ addProductFamilies <- function(rma) {
 }
 
 addUserDefs <- function(rma) {
-	so_master <- read.csv("H:/Code/supportFiles/so_User_Defs.csv")
+	so_master <- read.csv("Code/supportFiles/so_User_Defs.csv")
 	rma <- left_join(rma, so_master, by = c("orig_SO" = "Sales.Order.ID"))
 	rma$Specifier <- rma$Specifier.User.Def.3
 	rma$Job_Name <- rma$Job.Name.User.Def.5
