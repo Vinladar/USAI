7d0a29390135ef31cb7fe0cb894f8aabbad37859
Bryan Paulsen <bryanpaulsen@comcast.net>
2021-05-17 09:56:59 -0400

    Latest changes to the Metrics report script

--- Code/Scripts/metricsReport.R
+++ Code/Scripts/metricsReport.R
@@ -35,18 +35,17 @@ getDrivers <- function(rma) {
   driverIDs <- data.frame(do.call("rbind", strsplit(as.character(rmaDrivers$Item_ID), "-", fixed = TRUE)))
   driversFinal <- cbind(rmaDrivers, driverIDs)[, c(1, 2, 4, 5, 6, 10)]
   names(driversFinal) <- c("RMA_ID", "Item_ID", "Return_Qty", "Reason_Code", "Month", "DIM_Type")
-  groupedDrivers <- merge(driversFinal, driver_to_E2, by.x = c("Item_ID"), by.y = c("SP_Kit"), all.x = TRUE) %>%
+  groupedDrivers <- merge(driversFinal, driver_to_E2, by.x = c("Item_ID"), by.y = c("SP_Kit"), all.x = TRUE)
+  groupedDrivers1 <- groupedDrivers[, c(8, 2, 3, 4, 6, 10, 9, 11)] %>%
     group_by(E2, DIM_Type, E2_Acct_Val, SP_Acct_Val, SP_Price)
-  groupedDrivers1 <- groupedDrivers[, c(8, 2, 3, 4, 10, 9, 11)]
   groupedDrivers1$SP_Price <- as.numeric(groupedDrivers1$SP_Price)
   groupedDrivers1$SP_Price <- as.numeric(groupedDrivers1$SP_Acct_Val)
   groupedDrivers1$SP_Price <- as.numeric(groupedDrivers1$E2_Acct_Val)
   # Calculated values for SP and E2 cost vs price are not needed. 
   # groupedDrivers1$SP_Acct_Val <- groupedDrivers1$SP_Acct_Val * groupedDrivers1$Return_Qty
   # groupedDrivers1$E2_Acct_Val <- groupedDrivers1$E2_Acct_Val * groupedDrivers1$Return_Qty
-  groupedDrivers1$SP_Total_Price <- groupedDrivers1$SP_Price * groupedDrivers1$Return_Qty
-  groupedDrivers2 <- summarise(groupedDrivers1, "# of RMAs" = length(unique(RMA_ID)),  Qty = sum(Return_Qty), SP_Total_Price = sum(SP_Total_Price))
-  groupedDrivers2 <- groupedDrivers2[,c(5, 6, 1, 2, 3, 4, 7)]
+  groupedDrivers2 <- summarise(groupedDrivers1, "# of RMAs" = length(unique(RMA_ID)),  Qty = sum(Return_Qty))
+  groupedDrivers2 <- groupedDrivers2[, c(6, 7, 1, 2, 3, 4, 5)]
   # groupedDrivers1$CostToReplace <- groupedDrivers1$SP_Price - groupedDrivers1$SP_Acct_Val
   groupedDrivers2 <- arrange(groupedDrivers2, desc(Qty))
   write.csv(groupedDrivers2, driver_out)
@@ -63,8 +62,8 @@ getLightEngines <- function(rma) {
   # groupedEngines$LEM_Acct_Val <- groupedEngines$LEM_Acct_Val * groupedEngines$Return_Qty
   # groupedEngines$LED_Acct_Val <- groupedEngines$LED_Acct_Val * groupedEngines$Return_Qty
   # groupedEngines$LEM_Price <- groupedEngines$LEM_Price * groupedEngines$Return_Qty
-  groupedEngines1 <- summarize(groupedEngines, "# of RMAs" = length(unique(RMA_ID)),  Qty = sum(Return_Qty), Total_Price = sum(LEM_Total_Price))
-  groupedEngines1 <- arrange(groupedEngines1, desc(Qty))[, c(6, 7, 1, 2, 3, 4, 5, 8)]
+  groupedEngines1 <- summarize(groupedEngines, "# of RMAs" = length(unique(RMA_ID)),  Qty = sum(Return_Qty))
+  groupedEngines1 <- arrange(groupedEngines1, desc(Qty))[, c(6, 7, 1, 2, 3, 4)]
   # groupedEngines1$CostToReturn <- groupedEngines1$LEM_Price - groupedEngines1$LEM_Acct_Val
   write.csv(groupedEngines1, engines_out)
 }
